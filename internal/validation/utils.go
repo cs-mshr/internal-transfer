ff80c03926327ff9c64f5ec47e9a2e21de715dd9
Chandra-Shekhar <cshekharmshr2407@gmail.com>
2025-10-29 23:41:48 +0530

    (improve) error handling

--- internal/validation/utils.go
+++ internal/validation/utils.go
@@ -6,9 +6,9 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/chandra-shekhar/internal-transfers/internal/errs"
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
-	"github.com/chandra-shekhar/internal-transfers/internal/errs"
 )
 
 type Validatable interface {
